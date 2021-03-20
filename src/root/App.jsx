34eb98518a78b186d6ae6b1836cc6904913923b1
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-19 20:10:37 -0700

    feat: Process uploaded pdf file

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { HomePage, PDF } from '../components/pages';
+import GlobalStyles from './GlobalStyles';
 
 const App = () => {
-    return <div>test</div>;
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const pdf = useRef(null);
+    let url = null;
+
+    const submitPDF = () => {
+        pdf.current = pdf.current.files[0];
+        setIsSubmitted(true);
+    };
+
+    const goToHomePage = () => {
+        setIsSubmitted(false);
+    };
+
+    if (isSubmitted) {
+        const urlBlob = new Blob([pdf.current], { type: 'application/pdf' });
+        url = URL.createObjectURL(urlBlob);
+    }
+
+    return (
+        <>
+            <GlobalStyles />
+            {!isSubmitted && <HomePage onSubmit={submitPDF} inputRef={pdf} />}
+            {isSubmitted && <PDF titleName={pdf.current.name} onClick={goToHomePage} src={url} />}
+        </>
+    );
 };
 
 export default App;
