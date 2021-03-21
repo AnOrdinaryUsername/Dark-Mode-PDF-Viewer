c742dc204a3cf90302e09d8d5401d4bb07bdc8b4
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-20 20:57:04 -0700

    style: Change padding space on different viewports
    Center Title (h1) in Article.jsx for balance.

--- src/components/molecules/Article.jsx
+++ src/components/molecules/Article.jsx
@@ -6,12 +6,29 @@ const Container = styled.article`
     align-items: center;
     flex-direction: column;
     flex: 1;
+    padding-top: 0;
+    padding-bottom: 3.2rem;
+    padding-left: 3.2rem;
+    padding-right: 3.2rem;
+
+    @media (min-width: 576px) {
+        padding-bottom: 6.4rem;
+        padding-left: 6.4rem;
+        padding-right: 6.4rem;
+    }
+
+    @media (min-width: 768px) {
+        padding-bottom: 4.8rem;
+        padding-left: 4.8rem;
+        padding-right: 4.8rem;
+    }
 `;
 
 const StyledTitle = styled.h1`
+    display: inline;
+    margin: 0 auto;
     padding: 1.2rem;
     font-size: 2.5em;
-
     background-image: linear-gradient(
         transparent 0,
         transparent 55%,
@@ -21,12 +38,6 @@ const StyledTitle = styled.h1`
         transparent 100%
     );
     background-repeat: no-repeat;
-    display: inline;
-
-    @media (min-width: 576px) {
-        margin-left: 2.4rem;
-        margin-right: auto;
-    }
 `;
 
 const Article = ({ children, title }) => {
