3541a5e920678e2f9680d4d7be733a692fc6d0ce
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-21 12:19:05 -0700

    fix: Reduce PDF Canvas width on mobile portrait
    Remove height property in <Page /> since it doesn't do anything.
    From react-pdf docs: "If you define width and height at the same
    time, height will be ignored."

--- src/components/molecules/SectionBox/PreviewPDF.jsx
+++ src/components/molecules/SectionBox/PreviewPDF.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import styled from 'styled-components';
 import SectionBox from './SectionBox';
@@ -32,7 +32,42 @@ const Heading = styled.h2`
     padding-bottom: 1.6rem;
 `;
 
+// Custom hook from https://usehooks.com/useWindowSize/
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        // Add event listener
+        window.addEventListener('resize', handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+
+    return windowSize;
+}
+
 const PreviewPDF = ({ hue, invertValue, file }) => {
+    const size = useWindowSize();
+    const mobilePortrait = size.width < 456;
+
     return (
         <Box>
             <Heading>Preview</Heading>
@@ -45,7 +80,7 @@ const PreviewPDF = ({ hue, invertValue, file }) => {
             <Wrapper hue={hue} invert={invertValue}>
                 {file && (
                     <Document file={file}>
-                        <Page height={300} width={300} pageNumber={1} />
+                        <Page width={mobilePortrait ? 200 : 300} pageNumber={1} />
                     </Document>
                 )}
             </Wrapper>
