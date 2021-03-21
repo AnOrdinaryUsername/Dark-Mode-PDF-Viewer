826eb75c4713f22c20babe5e9d4dfd1143fb2575
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-03-20 21:06:16 -0700

    feat: Add new relative paths of files

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,6 +2,18 @@ import Article from './Article';
 import ButtonGroup from './ButtonGroup/ButtonGroup';
 import HeaderButtons from './ButtonGroup/HeaderButtons';
 import PDFButtons from './ButtonGroup/PDFButtons';
-import SectionBox from './SectionBox';
+import SettingButtons from './ButtonGroup/SettingButtons';
+import PreviewPDF from './SectionBox/PreviewPDF';
+import SectionBox from './SectionBox/SectionBox';
+import Settings from './SectionBox/Settings';
 
-export { Article, ButtonGroup, HeaderButtons, PDFButtons, SectionBox };
+export {
+    Article,
+    ButtonGroup,
+    HeaderButtons,
+    PDFButtons,
+    PreviewPDF,
+    SectionBox,
+    SettingButtons,
+    Settings,
+};
